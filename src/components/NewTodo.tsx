a9634b566df95f4506160fe8634b2deca57b4f2f
Irem Kenar <ikenar@kazandirio-dev.com>
2022-12-24 16:17:11 +0300

    added function prop & typescript

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,18 @@
 import { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const enteredText = todoTextInputRef.current?.value;
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+    props.onAddTodo(enteredText);
   };
 
   return (
